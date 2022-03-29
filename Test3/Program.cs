bd5f1ae992eacfc5ce57f5750617720e52d583e2
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-03-29 21:32:01 +0900

    혼자해보기 3번
    devkoboso.com/403
    혼자 해보기 3번 문제 풀이

--- Test3/Program.cs
+++ Test3/Program.cs
@@ -6,7 +6,22 @@ namespace Test3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            // 3. 1~ 20까지 수중에서 홀수 값의 합을 출력하세요
+
+            int sum = 0;
+            Console.Write("The Odd numbers are....");
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                    Console.Write($", {i}");
+                }
+            }
+
+            Console.WriteLine($"\nThe sum of odd numbers between 1 and 20 is.... {sum}");
         }
     }
 }
